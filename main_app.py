fe20f6a8c087ef97b0c6f6328a2292d143e7eccd
nzrjedGitHub <98401179+nzrjedGitHub@users.noreply.github.com>
2024-09-28 17:19:44 +0300

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,1 +1,10 @@
 # напиши тут свою програму
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.core.window import Window
+from kivy.uix.scrollview import ScrollView
+from instructions import txt_instruction, txt_test1, txt_test2, txt_test3, txt_sits
